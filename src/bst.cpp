e3f81ad4f94c8588df241b1287e5f505b55bf58f
donbool <benbenscher@gmail.com>
2022-04-13 15:11:49 -0400

    initial BST setup

---
+++ src/bst.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Node {
+    public:
+        Node* left;
+        Node* right;
+
+        Node() {
+            left = nullptr;
+            right = nullptr;
+        }
+
+        Node(string name, int id) {
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+class BST {
+    private:
+        Node* head = nullptr;
+        int height;
+        //Right rotate
+        //Left rotate   
+
+    public:
+        Node* insertNode(Node* node, int id, string name); //building the AVL ->
+        //insert helper function
+        void searchID(Node* root, int id);
+        void searchName(Node* root, string name, vector <long>& ids);
+
+        Node* returnRoot();
+        void setRoot(Node* node);
+
+        BST();
+        BST(Node* node);
+        ~BST();
+};
+
+//insert each of the rows, artwork nodes, from the csv
+Node* BST::insertNode(Node* node, int value, string value) { //CHANGE THE VALUE ACCORDING TO THE CSV
+
+    if (node == nullptr) {
+        return new Node(value, value);
+    }
+    else if (value == node->value) {
+    }
+    else if (value < node->value) {
+        node->left = insertNode(node->left, value, value);
+    }
+    else {
+        node->right = insertNode(node->right, value, value);
+    }
+    return node;
+}
+
+/****** search for the 3 different values below *****
+ * 
+ * values (name, id) are to be changed
+ * 
+ */
+
+void BST::searchID(Node* root, int id) {
+
+    Node* search = root;
+
+    while (search != nullptr) {
+        if (search->id == id) {
+            cout << search->name << endl;
+            break;
+        } 
+        else if (search->id < id) {
+            search = search->left;
+        } 
+        else {
+            search = search->right;
+        }
+    }
+
+}
+
+void BST::searchName(Node* root, string name, vector <long>& ids) { //return anything that matches
+
+    if (root != nullptr) {
+        if (root->name == name) {
+            ids.push_back(root->id);
+        }
+        searchName(root->left, name, ids);
+        searchName(root->right, name, ids);
+    }
+
+}
+
+void BST::searchName(Node* root, string name, vector <long>& ids) { //return anything that matches
+
+    if (root != nullptr) {
+        if (root->name == name) {
+            ids.push_back(root->id);
+        }
+        searchName(root->left, name, ids);
+        searchName(root->right, name, ids);
+    }
+
+}
