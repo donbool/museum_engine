0735de4ad6400e2954399d1c671b542180783443
w-omar <wocollado@gmail.com>
2022-04-16 11:26:57 -0400

    basis for terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include "artwork.cpp"
-#include "splay.cpp"
-#include "redblacktree.cpp"
+//#include "splay.cpp"
+//#include "redblacktree.cpp"
 #include <fstream>
 #include <vector>
+#include <chrono>
+#include <algorithm>
 using namespace std;
 
 /** PREMISE **
@@ -34,32 +36,67 @@ using namespace std;
 
 * Create PDF report!
 *****/
+void parseInput(string input, vector<ArtWork>& tree){
+	stringstream ss(input);
+	vector<string> fullCommand;
+	
+	while(ss.good()){
+		string commandWord;
+		getline(ss, commandWord, ' ');
+		fullCommand.push_back(commandWord);
+	}
+	
+	const vector<string> validCommands{"search", "remove", "insert", "recommend"};
 
-int main() {
+	auto keyword = find(validCommands.begin(), validCommands.end(), fullCommand.at(0));
+
+	if(keyword == validCommands.end()){
+		cout << "Invalid command" << endl;
+		return;
+	}
+	int commandIndex = keyword - validCommands.begin();
+	if(commandIndex == 0){
+		cout << "searching" << endl;
+	}
+	else if(commandIndex == 1){
+		cout << "removing" << endl;
+	}
+	else if(commandIndex == 2){
+		cout << "inserting" << endl;
+	}
+	else if(commandIndex == 3){
+		cout << "recommending" << endl;
+	}
+}
+void initRBT(vector<ArtWork>& tree){
 	ifstream input;
-	string line, function, title, id, link;
+	string line;
 
 	input.open("artData.csv");
 
 	getline(input, line);
-	vector<ArtWork> first10;
-	int x;
-	cin >> x;
-	for(int i = 0; i < x; i++){
-		getline(input, line);
+	auto start = chrono::high_resolution_clock::now();
+	int count = 0;
+	while(getline(input, line)){
+		count++;
+		tree.push_back(ArtWork(line));//tree.insert(ArtWork(line));
 	}
-	for(int i = 0; i < 100000; i++){
-		getline(input, line);
-		first10.push_back(ArtWork(line));	
-
-		cout << "==============================================" << endl;
-		cout << first10.back().getID() << endl;
-		cout << first10.back().getTitle() << endl;
-		cout << first10.back().getCulture() << endl;
-		cout << first10.back().getObjectYear() << endl;
-		cout << first10.back().getCountry() << endl;
-		cout << first10.back().getLink() << endl;
-		cout << i+x << endl;
-	}
+	auto stop = chrono::high_resolution_clock::now();
 	input.close();
+
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	cout << "Time taken for " << count << " insertions into RBT: " << duration.count() << " microseconds" <<  endl;
+}
+int main() {
+	cout << "Welcome to the Museum Engine" << endl;
+
+	vector<ArtWork> dummyContainer;
+	initRBT(dummyContainer);
+
+	string command;
+	getline(cin, command);
+	while(command != "quit"){
+		parseInput(command, dummyContainer);
+		getline(cin, command);
+	}
 }
